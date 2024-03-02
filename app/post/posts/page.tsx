28847671468329e2a929056d8c41ef3308cd056a
Andrew Pulyaev <andrew.pulyaev@gmail.com>
2024-03-02 12:39:33 +0200

    empty text is added to Posts page

--- app/post/posts/page.tsx
+++ app/post/posts/page.tsx
@@ -19,24 +19,34 @@ const Posts = async () => {
 
   const postsReverse: PostType[] = posts.reverse()
 
+  let isEmpty: boolean = false
+
+  if(postsReverse.length === 0) {
+    isEmpty = true
+  }
+
   return (
-    <ul className="post-lists__container h-[calc(100vh_-_64px_-_64px)] overflow-y-scroll">
-      {postsReverse.map(post => {
-        return (
-          <li key={post.id} className="post-lists__item">
-            <Post
-              id={post.id}
-              time={post.time}
-              summary={post.summary}
-              text={post.text}
-              type={post.type}
-              isFavorite={post.isFavorite}
-              page="lists"
-            />
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      {isEmpty && <p className="text-center text-4xl text-[#14077c]">No Posts are added to Favorites!</p>}
+
+      <ul className="post-lists__container h-[calc(100vh_-_64px_-_64px)] overflow-y-scroll">
+        {postsReverse.map(post => {
+          return (
+            <li key={post.id} className="post-lists__item">
+              <Post
+                id={post.id}
+                time={post.time}
+                summary={post.summary}
+                text={post.text}
+                type={post.type}
+                isFavorite={post.isFavorite}
+                page="lists"
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
