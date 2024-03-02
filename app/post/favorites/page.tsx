e0a552ac046caeb7af24844cee27dfe91cf38405
Andrew Pulyaev <andrew.pulyaev@gmail.com>
2024-03-02 12:37:15 +0200

    Favorits Posts page logik is added

--- app/post/favorites/page.tsx
+++ app/post/favorites/page.tsx
@@ -1,6 +1,54 @@
-const Page = () => {
-  return(
-    <div>Favorites post component</div>
+import {PostsData, PostType} from "@/utils/models";
+import {getData} from "@/utils/api";
+import Post from "@/components/Post";
+
+const Page = async () => {
+  const data: PostsData = await getData()
+  const posts: PostType[] = []
+
+  for (let key in data) {
+    if (data[key].isFavorite === true) {
+      posts.push({
+        id: key,
+        summary: data[key].summary,
+        text: data[key].text,
+        type: data[key].type,
+        time: data[key].time,
+        isFavorite: data[key].isFavorite
+      })
+    }
+  }
+
+  const postsReverse: PostType[] = posts.reverse()
+
+  let isEmpty: boolean = false
+
+  if(posts.length === 0) {
+    isEmpty = true
+  }
+
+  return (
+    <>
+      {isEmpty && <p className="text-center text-4xl text-[#14077c]">No Posts are added to Favorites!</p>}
+
+      <ul className="post-lists__container h-[calc(100vh_-_64px_-_64px)] overflow-y-scroll">
+        {postsReverse.map(post => {
+          return (
+            <li key={post.id} className="post-lists__item">
+              <Post
+                id={post.id}
+                time={post.time}
+                summary={post.summary}
+                text={post.text}
+                type={post.type}
+                isFavorite={post.isFavorite}
+                page="lists"
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
